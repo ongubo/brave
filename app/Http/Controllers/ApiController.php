754f2d419a6fe99a28e83e291dbdade94ebbb086
ongubo <developerworks25@gmail.com>
2019-01-16 20:45:04 +0300

    Add API functionality

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Libraries\Polyline;
+use App\Promocode;
+use Illuminate\Http\Request;
+
 class ApiController extends Controller
 {
     /**
@@ -11,30 +15,98 @@ class ApiController extends Controller
      */
     public function __construct()
     {
-        
-    }
-
-    public function generate_promo_code()
-    {
-        # code...
-    }
-
-    public function deactivate_promo_code()
-    {
-        # code...
+        // If auth is needed update constructor method
     }
 
     public function active_promo_codes()
     {
-        # code...
+        return response()->json(
+            [
+                'message' => 'Promo code fetched succesfully',
+                'data' => Promocode::join('status', 'status.id', 'promo_codes.status_id')
+                    ->join('events', 'events.id', 'promo_codes.event_id')
+                    ->select('promo_codes.id', 'events.name as event', 'promo_codes.code', 'status.name', 'promo_codes.value', 'promo_codes.radius', 'promo_codes.expires_at', 'promo_codes.created_at', 'promo_codes.updated_at')
+                    ->where('status_id', 1)
+                    ->get(),
+            ]
+        );
+
     }
-    public function all_promo_code()
+    public function all_promo_codes()
     {
-        # code...
+        return response()->json(
+            [
+                'message' => 'Promo code fetched succesfully',
+                'data' => Promocode::join('status', 'status.id', 'promo_codes.status_id')
+                    ->join('events', 'events.id', 'promo_codes.event_id')
+                    ->select('promo_codes.id', 'events.name as event', 'promo_codes.code', 'status.name', 'promo_codes.value', 'promo_codes.radius', 'promo_codes.expires_at', 'promo_codes.created_at', 'promo_codes.updated_at')
+                    ->get(),
+            ]
+        );
+
     }
 
-    public function update_promo_code()
+    public function redeem_promo_code(Request $request)
     {
-        # code...
+        $this->validate($request, [
+            // User details for authentication
+            'origin_lat' => 'required|numeric',
+            'origin_long' => 'required|numeric',
+            'destination_lat' => 'required|numeric',
+            'destination_long' => 'required|numeric',
+            // Promo code details
+            'promo_code' => 'required',
+        ]);
+        $promo_code = Promocode::join('status', 'status.id', 'promo_codes.status_id')
+            ->join('events', 'events.id', 'promo_codes.event_id')
+            ->select(
+                'promo_codes.id',
+                'events.name as event',
+                'promo_codes.code',
+                'status.name',
+                'promo_codes.value',
+                'promo_codes.radius',
+                'promo_codes.expires_at',
+                'promo_codes.created_at',
+                'promo_codes.updated_at')
+            ->where('code', $request->promo_code)
+            ->first();
+        $distance = $this->distance($request->origin_lat, $request->origin_long, $request->destination_lat, $request->destination_long);
+
+        $points = [
+            [$request->origin_lat, $request->origin_lon],
+            [$request->destination_lat, $request->destination_long],
+        ];
+
+        $poly_line = Polyline::Encode($points);
+
+        if ($distance <= $promo_code->radius) {
+            return response()->json(
+                [
+                    'message' => 'Promo code is valid',
+                    'promo_code' => $promo_code,
+                    'polyline' => $poly_line,
+                    'distance' => $distance,
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'message' => 'Promo code is invalid',
+                ]
+            );
+        }
+
     }
+
+    // Helper function to find the distance between two points given lat and long
+    public function distance($lat1, $lon1, $lat2, $lon2)
+    {
+        $theta = $lon1 - $lon2;
+        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        return $dist * 60 * 1.1515 * 1609.344; //Return distance in meters
+    }
+
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Event;
-use App\User;
 use App\Promocode;
-use Illuminate\Support\Facades\Hash;
+use App\User;
 use Carbon\Carbon;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use DB;
 
 class UserController extends Controller
 {
@@ -27,7 +27,7 @@ class UserController extends Controller
             'password' => 'required',
             // Promo code details
             'promo_code_expiry' => 'required|date',
-            'promo_code' => 'required',
+            'promo_code' => 'required|unique:promo_codes,code',
             'promo_code_radius' => 'required|numeric',
             'promo_code_value' => 'required|numeric',
             // Event details
@@ -40,29 +40,90 @@ class UserController extends Controller
 
         // Authenticate user
         $user = User::where('email', $request->email)->first();
-        
-        if ($user && Hash::check($request->password, $user->password)) {
-            // Create event
-            $event = new Event();
-            $event->name = $request->event_name;
-            $event->description = $request->event_description;
-            $event->lat = $request->lat;
-            $event->long = $request->long;
-            $event->starts_at = Carbon::parse($request->event_date);//'2016-12-20 10:26'
-            $event->save();
 
-            // Create promo code for event
-            $promo_code = new Promocode();
-            $promo_code->event_id = $event->id;
-            $promo_code->code = $request->promo_code;
-            $promo_code->radius = $request->promo_code_radius;
-            $promo_code->value = $request->promo_code_value;
-            $promo_code->expires_at = Carbon::parse($request->promo_code_expiry);
-            $promo_code->save();
-            
-            return response()->json(['status' => 'Event succesfully created']);
+        if ($user && Hash::check($request->password, $user->password)) {
+            // Use transaction so that iff a promocode is not created an event is also not created
+            DB::transaction(function () use ($request){
+                // Create event
+                $event = new Event();
+                $event->name = $request->event_name;
+                $event->description = $request->event_description;
+                $event->lat = $request->lat;
+                $event->long = $request->long;
+                $event->starts_at = Carbon::parse($request->event_date); //'2016-12-20 10:26'
+                $event->save();
+
+                // Create promo code for event
+                $promo_code = new Promocode();
+                $promo_code->event_id = $event->id;
+                $promo_code->status_id = 1;
+                $promo_code->code = $request->promo_code;
+                $promo_code->radius = $request->promo_code_radius;
+                $promo_code->value = $request->promo_code_value;
+                $promo_code->expires_at = Carbon::parse($request->promo_code_expiry);
+                $promo_code->save();
+                //
+            });
+
+            return response()->json(['message' => 'Event succesfully created']);
         } else {
-            return response()->json(['status' => 'Invalid credentials'], 401);
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+    }
+
+    public function deactivate_promo_code(Request $request)
+    {
+        $this->validate($request, [
+            // User detaiils for authentication
+            'email' => 'required',
+            'password' => 'required',
+            // Promo code details
+            'promo_code_id' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            // deactivate promocode
+            $promo_code = Promocode::find($request->promo_code_id);
+            if ($promo_code) {
+                $promo_code->status_id = 4;
+                $promo_code->save();
+                return response()->json(['message' => 'Promo code succesfully deactivated']);
+            } else {
+                return response()->json(['message' => 'Promocode not found'], 404);
+            }
+        } else {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+    }
+        public function update_promo_code_radius(Request $request)
+    {
+        $this->validate($request, [
+            // User detaiils for authentication
+            'email' => 'required',
+            'password' => 'required',
+            // Promo code details
+            'promo_code_id' => 'required',
+            'promo_code_radius' => 'required|numeric',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            // update promo code radius
+            $promo_code = Promocode::find($request->promo_code_id);
+            if ($promo_code) {
+                $promo_code->radius = $request->promo_code_radius;
+                $promo_code->save();
+                return response()->json(['message' => 'Promo code radius succesfully updated']);
+            } else {
+                return response()->json(['message' => 'Promocode not found'], 404);
+            }
+        } else {
+            return response()->json(['message' => 'Invalid credentials'], 401);
         }
 
     }
