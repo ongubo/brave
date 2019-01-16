28745a402f868bfa6887dc0a8b556310e4524160
ongubo <developerworks25@gmail.com>
2019-01-16 14:57:38 +0300

    Database structure and seed data

---
+++ database/migrations/2019_01_16_112344_create_initial_database.php
@@ -0,0 +1,90 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInitialDatabase extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        
+
+        Schema::create('status', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+        });
+
+
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('address')->nullable();
+            $table->float('lat', 10, 6);
+            $table->float('long', 10, 6);
+            $table->timestamps();
+        });
+
+        Schema::create('promo_codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('event_id')->unsigned();
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->string('name');
+            $table->float('value', 10, 2);
+            $table->float('radius', 10, 2);
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('user_promo_codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('promo_code_id')->unsigned();
+            $table->foreign('promo_code_id')->references('id')->on('promo_codes');
+            $table->integer('status_id')->unsigned();
+            $table->foreign('status_id')->references('id')->on('status');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('status');
+        Schema::dropIfExists('events');
+        Schema::dropIfExists('promo_codes');
+        Schema::dropIfExists('user_promo_codes');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        $this->call('DevelopmentTableSeeder');
+    }
+}
+
+class DevelopmentTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('roles')->insert(
+            [
+                ['name' => 'Admin'],
+                ['name' => 'User'],
+            ]
+        );
+
+        DB::table('users')->insert(
+            [
+                'firstName' => "Brave",
+                'lastName' => "Administrator",
+                'role_id' => 1,
+                'email' => 'admin@brave.com',
+                'password' => Hash::make('admin'),
+            ]
+        );
+
+        DB::table('status')->insert(
+            [
+                ['name' => 'ACTIVE', 'description' => ''],
+                ['name' => 'REDEEMED', 'description' => ''],
+                ['name' => 'EXPIRED', 'description' => ''],
+                ['name' => 'DEACTIVATED', 'description' => ''],
+            ]
+        );
+
     }
 }
